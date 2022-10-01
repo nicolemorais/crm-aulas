4d679978c396259d31d748d02445d1ff81e1fd11
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2022-09-30 21:36:21 -0300

    commit

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -1,16 +1,24 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Contato;
 
 class UsuarioModel extends Model {
     
     public function salvar($data) {
         // inserir no banco de dados
-        echo 'Dados inseridos no BD: <br>';
+       $contato['name'] = $data['name'];
+       $contato['email'] = $data['email'];
+       $contato['subject'] = $data['subject'];
+       $contato['message'] = $data['message'];
 
-        foreach($data as $key => $val) {
-            echo "$key => $val <br>";
-        }
+        // Quem vai inserir estes dados?
+        Contato::create($contato);
+
+        //Onde vamos inserir estes dados?
+       
+
+
     }
 
 }
